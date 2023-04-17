ec27208453afc9e9f2b1a4ec87d5a2e61ec88f9a
Mazarine Hajduk <hdx7z7pr6q47-student@anonymous-codio.com>
2023-04-17 17:25:00 +0000

    étape 6

--- parkingsystem/src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ parkingsystem/src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -2,6 +2,9 @@ package com.parkit.parkingsystem.integration;
 
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.FareCalculatorService;;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.service.ParkingService;
@@ -13,7 +16,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.when;
+import org.mockito.Mockito;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -22,6 +30,9 @@ public class ParkingDataBaseIT {
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
+    
+    @Mock
+    private static FareCalculatorService fareCalculatorService = new FareCalculatorService();
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
@@ -49,9 +60,13 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
+        int nbTicket = ticketDAO.getNbTicket("ABCDEF");
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        ParkingSpot parkingSpot = parkingService.getNextParkingNumberIfAvailable();
         parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        assertEquals(ticketDAO.getNbTicket("ABCDEF"), nbTicket + 1);
+        assertNotEquals(parkingService.getNextParkingNumberIfAvailable(), parkingSpot);
     }
 
     @Test
@@ -60,6 +75,31 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(ticket.getOutTime());
+        assertNotNull(ticket.getPrice());
+    }
+
+
+    public void testParkingLotExitRecurringUser() throws Exception{
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        // crée un ticket avec une entrée pour devenir un utilisateur récurrent
+        Ticket ticket = new Ticket();
+    
+        ticket.setVehicleRegNumber("ABCDEF");
+        ticket.setPrice(45);
+        ticket.setInTime(new Date(System.currentTimeMillis() - (24 * 60 * 60 * 1000))); // entrée il y a 10 heures
+        ticket.setOutTime(new Date(System.currentTimeMillis() - (22 * 60 * 60 * 1000))); // sortie il y a 9 heures
+        ticketDAO.saveTicket(ticket);
+        parkingService.processIncomingVehicle();
+
+        ticket = ticketDAO.getTicket("ABCDEF");
+        ticket.setInTime(new Date(System.currentTimeMillis() - (60 * 60 * 1000)));
+        ticketDAO.saveTicket(ticket);
+
+        parkingService.processExitingVehicle();
+ 
+        verify(fareCalculatorService, times (1)).calculateFare( any(Ticket.class), eq(true));
     }
 
 }
